b37f8777733e5c7f5f3f24e0486b37a0c276a4d9
Guillaume Pinot <texitoi@texitoi.eu>
2019-11-16 17:06:19 +0100

    Remove possible panics in layout (except if the layers are empty)
    fix #15

--- src/layout.rs
+++ src/layout.rs
@@ -30,17 +30,33 @@ impl Layout {
         kp: impl Iterator<Item = (usize, usize)> + Clone + 'a,
     ) -> impl Iterator<Item = KeyCode> + 'a {
         let layer = self.layer(kp.clone()).unwrap_or(self.default_layer);
-        kp.flat_map(move |(i, j)| match self.layers[layer][i][j] {
-            Action::Trans => self.layers[self.default_layer][i][j].key_codes(),
-            Action::DefaultLayer(default) => {
-                self.default_layer = default;
-                Action::DefaultLayer(default).key_codes()
+        kp.flat_map(move |(i, j)| {
+            let action = self
+                .layers
+                .get(layer)
+                .unwrap_or(&self.layers[self.default_layer])
+                .get(i)
+                .and_then(|c| c.get(j).copied());
+            match action {
+                None => Action::No.key_codes(),
+                Some(Action::Trans) => self.layers[self.default_layer]
+                    .get(i)
+                    .and_then(|c| c.get(j).copied())
+                    .unwrap_or(Action::No)
+                    .key_codes(),
+                Some(Action::DefaultLayer(default)) => {
+                    if default < self.layers.len() {
+                        self.default_layer = default;
+                    }
+                    Action::DefaultLayer(default).key_codes()
+                }
+                Some(kc) => kc.key_codes(),
             }
-            kc => kc.key_codes(),
         })
     }
     fn layer(&self, kp: impl Iterator<Item = (usize, usize)>) -> Option<usize> {
-        let mut iter = kp.filter_map(|(i, j)| self.layers[self.default_layer][i][j].layout());
+        let mut iter =
+            kp.filter_map(|(i, j)| self.layers[self.default_layer].get(i)?.get(j)?.layout());
         let first = iter.next()?;
         Some(first + iter.sum::<usize>())
     }
