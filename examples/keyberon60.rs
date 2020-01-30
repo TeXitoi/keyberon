5fe6c7abc69ba86a891434fa289f1059298f4994
Guillaume Pinot <guillaume.pinot@citodi.fr>
2020-01-30 09:02:12 +0100

    change keyberon60 layout

--- examples/keyberon60.rs
+++ examples/keyberon60.rs
@@ -75,24 +75,24 @@ impl_heterogenous_array! {
 const CUT: Action = m(&[LShift, Delete]);
 const COPY: Action = m(&[LCtrl, Insert]);
 const PASTE: Action = m(&[LShift, Insert]);
-const C_SP: Action = HoldTap(LCtrl, Space);
-const L1_ENTER: Action = LayerTap(1, Enter);
-const CSPACE: Action = m(&[LCtrl, Space]);
+const C_ENTER: Action = HoldTap(LCtrl, Enter);
+const L1_SP: Action = LayerTap(1, Space);
+const CENTER: Action = m(&[LCtrl, Enter]);
 
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers = &[
     &[
-        &[k(Grave),   k(Kb1),k(Kb2),k(Kb3), k(Kb4),k(Kb5),   k(Kb6),   k(Kb7),  k(Kb8), k(Kb9),  k(Kb0),   k(Minus)   ],
-        &[k(Tab),     k(Q),  k(W),  k(E),   k(R),  k(T),     k(Y),     k(U),    k(I),   k(O),    k(P),     k(LBracket)],
-        &[k(RBracket),k(A),  k(S),  k(D),   k(F),  k(G),     k(H),     k(J),    k(K),   k(L),    k(SColon),k(Quote)   ],
-        &[k(Equal),   k(Z),  k(X),  k(C),   k(V),  k(B),     k(N),     k(M),    k(Comma),k(Dot), k(Slash), k(Bslash)  ],
-        &[Trans,      Trans, k(LGui),k(LAlt),C_SP, k(LShift),k(RShift),L1_ENTER,k(RAlt),k(BSpace),Trans,   Trans      ],
+        &[k(Grave),   k(Kb1),k(Kb2),k(Kb3), k(Kb4),k(Kb5),   k(Kb6),   k(Kb7), k(Kb8), k(Kb9),  k(Kb0),   k(Minus)   ],
+        &[k(Tab),     k(Q),  k(W),  k(E),   k(R),  k(T),     k(Y),     k(U),   k(I),   k(O),    k(P),     k(LBracket)],
+        &[k(RBracket),k(A),  k(S),  k(D),   k(F),  k(G),     k(H),     k(J),   k(K),   k(L),    k(SColon),k(Quote)   ],
+        &[k(Equal),   k(Z),  k(X),  k(C),   k(V),  k(B),     k(N),     k(M),   k(Comma),k(Dot), k(Slash), k(Bslash)  ],
+        &[Trans,      Trans, k(LGui),k(LAlt),L1_SP,k(LShift),k(RShift),C_ENTER,k(RAlt),k(BSpace),Trans,   Trans      ],
     ], &[
         &[k(F1),k(F2),k(F3),     k(F4),k(F5),    k(F6),k(F7),      k(F8),  k(F9),    k(F10), k(F11),  k(F12)],
         &[Trans,Trans,Trans,     Trans,Trans,    Trans,Trans,      Trans,  k(Delete),Trans,  Trans,   Trans ],
         &[d(0), d(1), k(NumLock),Trans,k(Escape),Trans,k(CapsLock),k(Left),k(Down),  k(Up),  k(Right),Trans ],
         &[Trans,Trans,CUT,       COPY, PASTE,    Trans,Trans,      k(Home),k(PgDown),k(PgUp),k(End),  Trans ],
-        &[Trans,Trans,Trans,     Trans,CSPACE,   Trans,Trans,      Trans,  Trans,    Trans,  Trans,   Trans ],
+        &[Trans,Trans,Trans,     Trans,Trans,    Trans,Trans,      CENTER, Trans,    Trans,  Trans,   Trans ],
     ],
 ];
 
