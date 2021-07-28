2fa3d535b578c0402a04db636371bca672bcc12e
Antoni Simka <antonisimka.8@gmail.com>
2021-07-28 21:45:10 +0200

    Fully const layouts

--- keyberon-macros/tests/mod.rs
+++ keyberon-macros/tests/mod.rs
@@ -1,6 +1,7 @@
 extern crate keyberon_macros;
 use keyberon::action::{k, l, m, Action, Action::*, HoldTapConfig};
 use keyberon::key_code::KeyCode::*;
+use keyberon::layout::*;
 use keyberon_macros::layout;
 
 #[test]
@@ -20,22 +21,22 @@ fn test_layout_equality() {
     };
 
     #[rustfmt::skip]
-    pub static LAYERS_OLD: keyberon::layout::Layers = &[
-        &[
-            &[k(Tab),    k(Q), k(W), k(E), k(R), k(T),   k(Y), k(U), k(I),     k(O),   k(P),      k(BSpace)],
-            &[k(LCtrl),  k(A), k(S), k(D), k(F), k(G),   k(H), k(J), k(K),     k(L),   k(SColon), k(Quote) ],
-            &[k(LShift), k(Z), k(X), k(C), k(V), k(B),   k(N), k(M), k(Comma), k(Dot), k(Slash),  k(Escape)],
-            &[NoOp, NoOp, k(LGui), l(1), k(Space), k(Escape),   k(BSpace), S_ENTER, l(1), k(RAlt), NoOp, NoOp],
+    pub static LAYERS_OLD: Layers<NoCustom, 12, 4, 2> = [
+        [
+            [k(Tab),    k(Q), k(W), k(E), k(R), k(T),   k(Y), k(U), k(I),     k(O),   k(P),      k(BSpace)],
+            [k(LCtrl),  k(A), k(S), k(D), k(F), k(G),   k(H), k(J), k(K),     k(L),   k(SColon), k(Quote) ],
+            [k(LShift), k(Z), k(X), k(C), k(V), k(B),   k(N), k(M), k(Comma), k(Dot), k(Slash),  k(Escape)],
+            [NoOp, NoOp, k(LGui), l(1), k(Space), k(Escape),   k(BSpace), S_ENTER, l(1), k(RAlt), NoOp, NoOp],
         ],
-        &[
-            &[k(Tab),    k(Kb1), k(Kb2), k(Kb3), k(Kb4), k(Kb5),   k(Kb6),  k(Kb7),  k(Kb8), k(Kb9), k(Kb0), k(BSpace)],
-            &[k(LCtrl),  s!(Kb1), s!(Kb2), s!(Kb3), s!(Kb4), s!(Kb5),   s!(Kb6), s!(Kb7), s!(Kb8),  s!(Kb9), s!(Kb0), MultipleActions(&[k(LCtrl), k(Grave)])],
-            &[k(LShift), NoOp, NoOp, NoOp, NoOp, NoOp,   k(Left), k(Down), k(Up), k(Right), NoOp, s!(Grave)],
-            &[NoOp, NoOp, k(LGui), Trans, Trans, Trans,   Trans, Trans, Trans, k(RAlt), NoOp, NoOp],
+        [
+            [k(Tab),    k(Kb1), k(Kb2), k(Kb3), k(Kb4), k(Kb5),   k(Kb6),  k(Kb7),  k(Kb8), k(Kb9), k(Kb0), k(BSpace)],
+            [k(LCtrl),  s!(Kb1), s!(Kb2), s!(Kb3), s!(Kb4), s!(Kb5),   s!(Kb6), s!(Kb7), s!(Kb8),  s!(Kb9), s!(Kb0), MultipleActions(&[k(LCtrl), k(Grave)])],
+            [k(LShift), NoOp, NoOp, NoOp, NoOp, NoOp,   k(Left), k(Down), k(Up), k(Right), NoOp, s!(Grave)],
+            [NoOp, NoOp, k(LGui), Trans, Trans, Trans,   Trans, Trans, Trans, k(RAlt), NoOp, NoOp],
         ],
     ];
 
-    pub static LAYERS: keyberon::layout::Layers = layout! {
+    pub static LAYERS: Layers<NoCustom, 12, 4, 2> = layout! {
         {
             [ Tab    Q W E R T   Y U I O P BSpace ]
             [ LCtrl  A S D F G   H J K L ; Quote  ]
@@ -52,17 +53,17 @@ fn test_layout_equality() {
 
     assert_eq!(LAYERS, LAYERS_OLD);
     use std::mem::size_of_val;
-    assert_eq!(size_of_val(LAYERS), size_of_val(LAYERS_OLD))
+    assert_eq!(size_of_val(&LAYERS), size_of_val(&LAYERS_OLD))
 }
 
 #[test]
 fn test_nesting() {
-    static A: keyberon::layout::Layers = layout! {
+    static A: Layers<NoCustom, 2, 1, 1> = layout! {
         {
             [{k(D)} [(5) [C {k(D)}]]]
         }
     };
-    static B: keyberon::layout::Layers = &[&[&[
+    static B: Layers<NoCustom, 2, 1, 1> = [[[
         k(D),
         Action::MultipleActions(&[Action::Layer(5), Action::MultipleActions(&[k(C), k(D)])]),
     ]]];
@@ -71,20 +72,28 @@ fn test_nesting() {
 
 #[test]
 fn test_layer_switch() {
-    static A: keyberon::layout::Layers = layout! {
+    static A: Layers<NoCustom, 5, 1, 1> = layout! {
         {
-            [(0xa), (0b0110), (b'a' as usize), (1 + 8 & 32), ([4,5][0])]
+            [(0xa) (0b0110) (b'a' as usize) (1 + 8 & 32) ([4,5][0])]
         }
     };
+    static B: Layers<NoCustom, 5, 1, 1> = [[[
+        Action::Layer(0xa),
+        Action::Layer(6),
+        Action::Layer(b'a' as usize),
+        Action::Layer(1 + 8 & 32),
+        Action::Layer([4, 5][0]),
+    ]]];
+    assert_eq!(A, B);
 }
 
 #[test]
 fn test_escapes() {
-    static A: keyberon::layout::Layers = layout! {
+    static A: Layers<NoCustom, 2, 1, 1> = layout! {
         {
             ['\\' '\'']
         }
     };
-    static B: keyberon::layout::Layers = &[&[&[k(Bslash), k(Quote)]]];
+    static B: Layers<NoCustom, 2, 1, 1> = [[[k(Bslash), k(Quote)]]];
     assert_eq!(A, B);
 }
